6f480c4312cca90bedbd570164d46d230b746351
bichengxiao16@gmail.com <bichengxiao16@gmail.com>
2019-03-02 23:16:38 -0500

    changed package.json server.js

--- slashq-ng/server.js
+++ slashq-ng/server.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/slshq-ng'));
+app.use(express.static(__dirname + '/dist/slashq-ng'));
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/slshq-ng/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/slashq-ng/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
